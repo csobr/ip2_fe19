3896e5213ccfa7faa57f5067aec5ee61a0b977c9
Siham <siham.hadi@live.se>
2019-12-16 20:49:28 +0100

    Template for projects + new folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
-function Home() {
-  return <div>Welcome to Next.js!</div>;
-}
+import React, { Component } from "react";
+import projects from '../data/projects.json';
 
-export default Home;
+export default class Projects extends Component {
+  renderProjects() {
+    return projects.map((project, index) => {
+      const { name, backgroundImage, year, description, stack } = project;
+      return (
+        <div key={name}>
+          <div className="item">
+            {name}
+            <div className="item--image">
+              <img src={backgroundImage} alt="Project Image"></img>
+            </div>
+            <div className="item--text">
+              <p className ="item--year">{year}</p>
+              <p className="item--description">{description}</p>
+              <p className="item--stack">{stack}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div className="projects--wrapper">
+        <div className="grid">{this.renderProjects()}</div>
+      </div>
+    );
+  }
+}
