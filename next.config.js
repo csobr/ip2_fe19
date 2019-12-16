57bc1726a2e7df11ef2cbb72ca035b7751b1b74e
Siham <siham.hadi@live.se>
2019-12-16 10:28:36 +0100

    New folder

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withSass = require("@zeit/next-sass");
+
+module.exports = {
+  devIndicators: {
+    autoPrerender: false
+  }
+};
+
+
+
 module.exports = withSass({
   /* config options here */
 });
