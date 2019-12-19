8e45cd91124e000fe1147719bb41f6fc4608c545
Siham <siham.hadi@live.se>
2019-12-19 21:38:02 +0100

    updated pagination

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,21 +2,21 @@ import React from "react";
 
 const Pagination = ({ projectsPerPage, totalProjects, paginate }) => {
   const pageNumbers = [];
-
   for (let i = 1; i <= Math.ceil(totalProjects / projectsPerPage); i++) {
     pageNumbers.push(i);
   }
+
   return (
     <nav>
-      <ul className="pagination">
+      <div className="pagination">
         {pageNumbers.map(number => (
-          <li key={number} className="dots">
-            <a onClick={() => paginate(number)} className="">
+          <div key={number} className="dots">
+            <button onClick={() => paginate(number)} className="dot">
               {number}
-            </a>
-          </li>
+            </button>
+          </div>
         ))}
-      </ul>
+      </div>
     </nav>
   );
 };
